7d03959a746c9f081de3d485baa9cae6297fd4e2
Jackson Peak <jacksonpeak1@icloud.com>
2021-04-28 10:10:57 -0400

    created index.js file under controller folder, and set up index.js, recipe-routes.js under api folder

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const recipeRoutes = require('./recipe-routes');
+const userRoutes = require('./user-routes');
+
+
+router.use('/recipe', recipeRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
