abc31ae817cabb4332f060adf60cd83a8d74a437
Clare Henderson <henderson2350@gmail.com>
2021-04-29 14:04:28 -0400

    side bar added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,7 +15,7 @@ router.get("/", async (req, res) => {
       return recipe.get({ plain: true });
     });
 
-    res.render("homepage", {
+    res.render("explore", {
       recipes,
       loggedIn: req.session.logged_in,
     });
