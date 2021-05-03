19bff3294ccc504c3c66aeb64dd1d19eb4761e13
DINNERman <shrwnwilliams@gmail.com>
2021-05-02 20:12:40 -0400

    made it so when you follow someone, you're taken to your profile and removed commented out code

--- public/js/profile.js
+++ public/js/profile.js
@@ -8,10 +8,8 @@ const followButtonHandler = async (event) => {
       body: JSON.stringify({ id }),
       headers: { "Content-Type": "application/json" },
     });
-
     if (response.ok) {
       document.location.replace('/myprofile');
-      // console.log("good job");
     } else {
       alert("Failed to follow");
     }
